88da735810cd8e40f80e94f3d067fbd16fc8f350
xamartinez <xa.martinez@alumnos.upm.es>
2018-03-23 10:40:45 +0100

    primer fichero

---
+++ Ejercicio2.c
@@ -0,0 +1,15 @@
+// Ejercicio 2 - Tema 3
+
+#include<stdio.h>
+int main()
+{
+	int x, y;
+	printf("Introduzca 2 numeros enteros:\n");
+	scanf("%d %d",&x,&y);
+	if(x % y ==0)
+	printf("El numero %d es divisible entre %d\n",x,y);
+	else
+	printf("Los numeros no son divisibles\n");
+	
+	return 0;
+}
