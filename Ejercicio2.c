d4967a705edbfbdc0e25d5abd6c1a83b4ba31f87
xamartinez <xa.martinez@alumnos.upm.es>
2018-03-23 10:42:00 +0100

    Añado comentarios

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -5,6 +5,7 @@ int main()
 {
 	int x, y;
 	printf("Introduzca 2 numeros enteros:\n");
+	// Uso scanf con ampersand
 	scanf("%d %d",&x,&y);
 	if(x % y ==0)
 	printf("El numero %d es divisible entre %d\n",x,y);
